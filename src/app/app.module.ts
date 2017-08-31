961ab6f21adb0ec5f42d467acbb91ac7bf2d80ed
Karim KOLLI <kakolli@airfrance.fr>
2017-08-31 18:58:16 +0200

    Add SpeechRecognition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { HttpModule } from '@angular/http';
+import {SpeechRecognition} from '@ionic-native/speech-recognition';
 import { MyApp } from './app.component';
 
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
@@ -35,6 +36,7 @@ import { CarServiceProvider } from '../providers/car-service/car-service';
   providers: [
     StatusBar,
     SplashScreen,
+    SpeechRecognition,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CarServiceProvider
   ]
--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
 
+import { NavController } from 'ionic-angular';
+import {SpeechRecognition} from '@ionic-native/speech-recognition';
+
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(public speech: SpeechRecognition, public navCtrl: NavController) {
 
+  }
+
+
+  listenForSpeech():void {
+    this.speech.startListening().subscribe(data => console.log("Speech :"+data), error => console.error("Speech error :"+error));
+  }
+
+
+  async hasPermissions():Promise<boolean> {
+    try {
+        const hasPermission =  await this.speech.hasPermission();
+      console.log("has Permissions :"+hasPermission);
+      return hasPermission;
+    } catch (error) {
+      console.error("has Permissions error:"+error);  
+    }
+    
+  }
+
+  async getPermissions():Promise<void> {
+    try {
+        const permission =  await this.speech.requestPermission();
+      console.log("Permissions :"+permission);
+    } catch (error) {
+      console.error("get Permissions error:"+error);  
+    }
+    
+  }
+
+
+   async isSpeechSupported():Promise<boolean> {
+    const isSpeechAvailable =  await this.speech.isRecognitionAvailable();
+    console.log("Speech available :"+isSpeechAvailable);
+    return isSpeechAvailable;
   }
 }
